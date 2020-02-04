5a9da6a08fff53ca3c79066c285df7c7af6a2293
oscaresluciano <oscaresluciano@outlook.com>
2020-02-03 22:13:06 -0300

    Ejercicios 1 y 2 resueltos

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,12 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+
+    //Solicito el nombre por prompt
+    nombre = prompt("Ingrese su nombre");
+
+    //Muestro el nombre por alert
+    alert(nombre);
 }
 
