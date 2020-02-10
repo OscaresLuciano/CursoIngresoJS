60d0d29c56014a845ec1378cbd6d84062a1d6608
Oscares Luciano <oscaresluciano@outlook.com>
2020-02-10 19:35:55 -0300

    ejercicios 1 y 2 de TPs resueltos

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -4,15 +4,51 @@ B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un ter
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
+var largo;
+var ancho;
+var radio;
+var resultado;
+var circular;
+var cemento;
+var cal;
+
 function Rectangulo () 
 {
+    largo = document.getElementById("Largo").value;
+    ancho = document.getElementById("Ancho").value;
 
+    largo = parseInt(largo);
+    ancho = parseInt(ancho);
+
+    resultado = largo * ancho;
+
+    console.log("Se necesitan " + resultado + " metros de alambre")
 }
 function Circulo () 
 {
-	
+	radio = document.getElementById("Radio").value;
+    radio = parseInt(radio);
+
+    circular = 2 * Math.PI * radio
+
+    console.log("Se necesitan " + circular + " metros de alambre")
 }
 function Materiales () 
 {
-	
+	largo = document.getElementById("Largo").value;
+    ancho = document.getElementById("Ancho").value;
+
+    largo = parseInt(largo);
+    ancho = parseInt(ancho);
+
+    cemento = 2
+    cal = 3
+
+    resultado = largo * ancho;
+
+    cemento = 2 * resultado;
+    cal = 3 * resultado;
+
+    console.log("Se necesitan " + cemento + " bolsas de cemento y " + cal + " de cal")
+
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,41 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
+    var precio1;
+    var precio2;
+    var precio3;
+    var resultado;
+
+function obtenerPrecios(){
+
+    precio1 = document.getElementById("PrecioUno").value;
+    precio2 = document.getElementById("PrecioDos").value;
+    precio3 = document.getElementById("PrecioTres").value;
+
+    precio1 = parseInt(precio1);
+    precio2 = parseInt(precio2);
+    precio3 = parseInt(precio3);
+}
+
 function Sumar () 
 {
-	
+	obtenerPrecios();
+
+    resultado = (precio1 + precio2 + precio3);
+    alert("El resultado de la suma es: " + resultado);
 }
 function Promedio () 
 {
-	
+	obtenerPrecios();
+
+    resultado = (precio1 + precio2 + precio3)/3;
+    alert("El promedio es: " + resultado);
 }
 function PrecioFinal () 
 {
-	
+	obtenerPrecios();
+
+    resultado = (precio1 + precio2 + precio3) * 1.21;
+    alert("El precio final es: " + resultado);
 }
